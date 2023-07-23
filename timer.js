521caf840dd8c5b482b939e3329599c937081c55
KNiC.xo <ntcaliwag@hotmail.com>
2023-07-22 21:01:49 -0700

    Set limits for inputs when typing

--- timer.js
+++ timer.js
@@ -7,5 +7,26 @@ minuteInput.value = '0';
 secondInput = document.querySelector('.js-second');
 secondInput.value = '0';
 
+const setMax = (element, number) => {
+  element.value = Number(element.value);
 
-//hourInput.addEventListener('keydown', setMax);
+  if (Number(element.value) > number) {
+    element.value = number.toString();
+  }
+
+  if (!element.value) {
+    element.value = '0';
+  }
+};
+
+hourInput.addEventListener('input', () => {
+  setMax(hourInput, 23);
+});
+
+minuteInput.addEventListener('input', () => {
+  setMax(minuteInput, 59);
+});
+
+secondInput.addEventListener('input', () => {
+  setMax(secondInput, 59);
+});
