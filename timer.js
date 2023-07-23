675daf7a371e8e0d3bcca6af93e7f21bc8c546ec
KNiC.xo <ntcaliwag@hotmail.com>
2023-07-22 20:12:31 -0700

    Added number inputs and basic styling

---
+++ timer.js
@@ -0,0 +1,11 @@
+hourInput = document.querySelector('.js-hour');
+hourInput.value = '0';
+
+minuteInput = document.querySelector('.js-minute');
+minuteInput.value = '0';
+
+secondInput = document.querySelector('.js-second');
+secondInput.value = '0';
+
+
+//hourInput.addEventListener('keydown', setMax);
