0228e06fa49cd86b7591aca321bd1e1cc4af0083
KNiC.xo <ntcaliwag@hotmail.com>
2023-07-24 06:35:00 -0700

    Added buttons and the countdown functionality

--- timer.js
+++ timer.js
@@ -1,11 +1,26 @@
 hourInput = document.querySelector('.js-hour');
-hourInput.value = '0';
+hourInput.value = '2';
 
 minuteInput = document.querySelector('.js-minute');
 minuteInput.value = '0';
 
 secondInput = document.querySelector('.js-second');
-secondInput.value = '0';
+secondInput.value ='10';
+
+startStopButton = document.querySelector('.js-start-stop');
+resetButton = document.querySelector('.js-reset');
+
+// Used to check if stopwatch is running
+let isOn = false;
+
+// Keeps ID of setInterval function for clearing
+let intervalID;
+
+// Stores total time elapsed for when stopwatch is paused
+let timeBank = 0;
+
+// Stores time elapsed while stopwatch is running
+let timeElapsed = 0;
 
 const setMax = (element, number) => {
   element.value = Number(element.value);
@@ -19,6 +34,21 @@ const setMax = (element, number) => {
   }
 };
 
+const countDown = (hours, minutes, seconds) => {
+  let totalTime = ((Number(hours.value) * 3600) + (Number(minutes.value) * 60) + Number(seconds.value) + 1) * 1000;
+  let startTime = Date.now();
+  intervalID = setInterval(() => {
+    timeElapsed = Date.now() - startTime;
+    //timeElapsed += 2000;
+    //console.log(totalTime - timeElapsed);
+    console.log((totalTime - timeElapsed) / 1000);
+    hours.value = Math.floor((totalTime - timeElapsed) / 3600000);
+    minutes.value = Math.floor((((totalTime - timeElapsed) / 1000) / 60) % 60);
+    seconds.value = Math.trunc(((totalTime - timeElapsed) / 1000) % 60);
+  }, 1);
+};
+
+
 hourInput.addEventListener('input', () => {
   setMax(hourInput, 23);
 });
@@ -30,3 +60,7 @@ minuteInput.addEventListener('input', () => {
 secondInput.addEventListener('input', () => {
   setMax(secondInput, 59);
 });
+
+startStopButton.addEventListener('click', () => {
+  countDown(hourInput, minuteInput, secondInput);
+})
